ecec417b2b408268da85a0829a7f99a23d07a417
w1nsun <ilya.nevmyvaka@gmail.com>
2017-02-28 18:17:14 +0200

    add fb login

--- src/UsersBundle/Controller/Frontend/RegistrationController.php
+++ src/UsersBundle/Controller/Frontend/RegistrationController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace UsersBundle\Controller\Frontend;
+use Facebook\Facebook;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use UsersBundle\Entity\User;
 use UsersBundle\Form\RegisterType;
 use UsersBundle\Model\RegisterModel;
 
@@ -15,6 +15,7 @@ class RegistrationController extends Controller
         $form = $this->createForm(RegisterType::class, $registerModel);
 
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $password = $this
                             ->get('security.password_encoder')
@@ -26,9 +27,24 @@ class RegistrationController extends Controller
             return $this->redirectToRoute('homepage');
         }
 
+        $fb = new Facebook([
+            'app_id' => $this->container->getParameter('facebook_app_id'),
+            'app_secret' => $this->container->getParameter('facebook_app_secret'),
+            'default_graph_version' => 'v2.2',
+        ]);
+
+        $helper = $fb->getRedirectLoginHelper();
+        $permissions = ['email'];
+        $loginUrl = $helper->getLoginUrl($this->generateUrl('users_registration_fb_callback'), $permissions);
 
         return $this->render('UsersBundle:Registration:register.html.twig', [
             'form' => $form->createView(),
+            'fb_login_url' => htmlspecialchars($loginUrl),
         ]);
+    }
+
+    public function fbCallbackAction(Request $request)
+    {
+        var_dump($request);
     }
 }
