3d58b61c89a5bec72d03f3cb295bf4d58123d18f
w1nsun <ilya.nevmyvaka@gmail.com>
2017-04-18 13:21:19 +0300

    fb login

--- src/UsersBundle/Controller/Frontend/SecurityController.php
+++ src/UsersBundle/Controller/Frontend/SecurityController.php
@@ -2,9 +2,13 @@
 
 namespace UsersBundle\Controller\Frontend;
 
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Facebook;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use UsersBundle\Entity\User;
 use UsersBundle\Form\RegistrationType;
 use UsersBundle\Repository\UserRepository;
@@ -21,9 +25,22 @@ class SecurityController extends Controller
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        $fb = new Facebook([
+            'app_id' => $this->container->getParameter('facebook_app_id'),
+            'app_secret' => $this->container->getParameter('facebook_app_secret'),
+            'default_graph_version' => 'v2.5',
+        ]);
+
+        $helper = $fb->getRedirectLoginHelper();
+        $loginUrl = $helper->getLoginUrl(
+            $this->generateUrl('users_security_fb_callback', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            ['email']
+        );
+
         return $this->render('UsersBundle:Security:login.html.twig', array(
             'last_username' => $lastUsername,
             'error'         => $error,
+            'fb_login_url'  => $loginUrl,
         ));
     }
 
@@ -49,5 +66,38 @@ class SecurityController extends Controller
     public function loginCheckAction()
     {
         return $this->redirectToRoute('homepage');
+    }
+
+    public function fbCallbackAction(Request $request)
+    {
+        $fb = new Facebook([
+            'app_id' => $this->container->getParameter('facebook_app_id'),
+            'app_secret' => $this->container->getParameter('facebook_app_secret'),
+            'default_graph_version' => 'v2.5',
+        ]);
+
+        $helper = $fb->getRedirectLoginHelper();
+        try {
+            $accessToken = $helper->getAccessToken();
+        } catch(FacebookResponseException $e) {
+            // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch(FacebookSDKException $e) {
+            // When validation fails or other local issues
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+
+        if (isset($accessToken)) {
+            // Logged in!
+            $_SESSION['facebook_access_token'] = (string) $accessToken;
+
+            // Now you can redirect to another page and use the
+            // access token from $_SESSION['facebook_access_token']
+        }
+
+        dump($accessToken);
+        exit;
     }
 }
