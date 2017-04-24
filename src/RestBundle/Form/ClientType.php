dc6aafce5e81f39babbf6883bcb3859a0a570a28
w1nsun <ilya.nevmyvaka@gmail.com>
2017-04-24 13:33:23 +0300

    add client form

--- src/RestBundle/Form/ClientType.php
+++ src/RestBundle/Form/ClientType.php
@@ -6,6 +6,7 @@ use RestBundle\Entity\Client;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -34,6 +35,9 @@ class ClientType extends AbstractType
             ])
             ->add('isActive', CheckboxType::class, [
                 'label' => 'Active ?',
+                'attr' => [
+                    'class' => 'js-switch',
+                ],
             ]);
     }
 
